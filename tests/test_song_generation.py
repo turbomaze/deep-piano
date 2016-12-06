df5b2a79603548769c2989a0557b99f71ccafc62
Anthony Liu <anthonyscottliu@gmail.com>
2016-12-06 01:39:59 -0500

    Removes gitignored files.

--- tests/test_song_generation.py
+++ tests/test_song_generation.py
@@ -1,5 +1,5 @@
 from context import deeppiano as dp
 
 song = dp.generate_song(4, 1, 0.5)
-file_name = '../data/songs/song_test.wav'
+file_name = '../data/test-songs/song_test.wav'
 dp.save_timeline_to_wav(song, file_name)
