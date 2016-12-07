d0d1cf23bd8cd363e1e8d60b6c80ad52dcc4e7cf
Anthony Liu <anthonyscottliu@gmail.com>
2016-12-07 18:25:14 -0500

    Fleshes out a function that compares the encodings of two songs.

--- deeppiano/imagination.py
+++ deeppiano/imagination.py
@@ -1,21 +1,36 @@
+import numpy as np
+from context import deeppiano as dp
+
+
 class Imagination(object):
     def __init__(self, autoencoder, frame_width):
-        self.encoder = autoencoder.encoder
-        self.frame_width
+        self.encoder = autoencoder.get_encoding
+        self.frame_width = frame_width
 
-    # given a .wav file location, returns its musical HLR
-    def infer_hlr(self, file_name):
-        # target song = open file_name
+    # given .wav data, returns its musical HLR
+    def infer_hlr(self, song_wav):
         # initial song = self.sample_song()
         # while get_distance(initial song, target song) is large
         # transition initial song
         pass
 
-    # given two .wav files, returns the MSE of their encodings
+    # given two .wav datas, returns the MSE of their encodings
     def get_distance(self, wav_a, wav_b):
+        # get their spectrograms
+        vectors_a = dp.get_vectors_from_data(
+            wav_a, self.frame_width
+        )
+        vectors_b = dp.get_vectors_from_data(
+            wav_b, self.frame_width
+        )
+
         # encode wav a and b with the autoencoder
+        encoding_a = self.encoder(vectors_a)
+        encoding_b = self.encoder(vectors_b)
+
         # return their squared difference sum
-        pass
+        mse = np.sum(np.square(encoding_b - encoding_a))
+        return mse
 
     # returns a randomly sampled HLR
     def sample_song(self):
