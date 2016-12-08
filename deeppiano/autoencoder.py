e8f0ebf62f63ed93342f27a5b38f083741847b21
Anthony Liu <anthonyscottliu@gmail.com>
2016-12-08 03:20:11 -0500

    Generalizes the autoencoder model to support deep autoencoders.

--- deeppiano/autoencoder.py
+++ deeppiano/autoencoder.py
@@ -6,20 +6,13 @@ class Autoencoder(object):
     def __init__(self, model_file_name):
         self.model = load_model(model_file_name)
 
-        # hardcodes the idea of a shallow autoencoder
+        num_layers = len(self.model.layers)
+        middle_layer = int(num_layers/2)
         self.encoder = K.function(
             [self.model.layers[0].input],
-            [self.model.layers[1].output]
-        )
-        self.decoder = K.function(
-            [self.model.layers[1].output],
-            [self.model.layers[2].output]
+            [self.model.layers[middle_layer].output]
         )
 
     def get_encoding(self, vectors):
         # activate the network until the smallest hidden layer
         return self.encoder([vectors])[0]
-
-    def get_decoding(self, encoding):
-        # activate all layers after the smallest hidden layer
-        return self.decoder([encoding])[0]
